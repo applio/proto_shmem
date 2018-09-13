0fe77e7ff49a266934369f86af237ffd743fb7a3
Davin Potts <davin@appliomics.com>
2018-09-12 19:33:51 -0500

    Added ShareableList class.

--- shared_memory.py
+++ shared_memory.py
@@ -1,6 +1,7 @@
 import mmap
 import os
 import random
+import struct
 import sys
 try:
     from posix_ipc import SharedMemory as _PosixSharedMemory, Error, ExistentialError, O_CREX
@@ -69,3 +70,88 @@ class SharedMemory:
         else:
             cls = PosixSharedMemory
         return cls(*args, **kwargs)
+
+
+class ShareableList:
+    # TODO: Adjust for discovered word size of machine.
+    types_mapping = {
+        int: "q",
+        float: "d",
+        bool: "xxxxxxx?",
+        str: "%ds",
+        bytes: "%ds",
+        None.__class__: "xxxxxx?x",
+    }
+    alignment = 8
+    encoding = "utf8"
+
+    def __init__(self, iterable):
+        self._formats = [
+            self.types_mapping[type(item)] if not isinstance(item, (str, bytes))
+                else self.types_mapping[type(item)] % (
+                    self.alignment * (len(item) // self.alignment + 1),
+                )
+            for item in iterable
+        ]
+        self._back_transforms = [
+            (type(item), len(item)) if isinstance(item, (str, bytes))
+                else None
+            for item in iterable
+        ]
+        self._len = len(self._formats)
+        self.shm = SharedMemory(None, size=struct.calcsize(self.format))
+        _enc = self.encoding
+        struct.pack_into(
+            self.format,
+            self.shm.buf,
+            0,
+            *(v.encode(_enc) if isinstance(v, str) else v for v in iterable),
+        )
+
+    def __getitem__(self, position):
+        try:
+            offset = sum(
+                self.alignment if fmt[-1] != "s" else int(fmt[:-1])
+                for fmt in self._formats[:position]
+            )
+        except IndexError:
+            raise IndexError("index out of range")
+        (v,) = struct.unpack_from(self._formats[position], self.shm.buf, offset)
+        if isinstance(v, bytes):
+            transform_type, transform_len = self._back_transforms[position]
+            if transform_type == str:
+                v = transform_type(v[:transform_len], encoding=self.encoding)
+            else:
+                v = transform_type(v[:transform_len])
+        return v
+
+    def __setitem__(self, position, value):
+        try:
+            offset = sum(
+                self.alignment if fmt[-1] != "s" else int(fmt[:-1])
+                for fmt in self._formats[:position]
+            )
+        except IndexError:
+            raise IndexError("assignment index out of range")
+        current_format = self._formats[position]
+        if not isinstance(value, (str, bytes)):
+            new_format = self.types_mapping[type(item)]
+            new_back_transform = None
+        elif current_format[-1] == "s":
+            if int(current_format[:-1]) < len(value):
+                raise ValueError("exceeds available storage for existing str")
+            new_format = current_format
+            new_back_transform = (type(value), len(value))
+        else:
+            if len(value) > self.alignment:
+                raise ValueError("str exceeds available storage")
+            new_format = current_format
+            new_back_transform = (type(value), len(value))
+        struct.pack_into(new_format, self.shm.buf, offset, value)
+        self._formats[position] = new_format
+        self._back_transforms[position] = new_back_transform
+
+    @property
+    def format(self):
+        return "".join(self._formats)
+    

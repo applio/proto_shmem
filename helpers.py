3f766749e516c31192d10bbe2787d9b703f8c067
Davin Potts <davin@appliomics.com>
2018-09-18 23:20:01 -0500

    Added helper for sharing pandas DataFrame columns.

---
+++ helpers.py
@@ -0,0 +1,45 @@
+from shared_memory import shareable_wrap
+
+
+def share_DataFrame_column(df, column):
+    """Because pandas uses a BlockManager to oversee the numpy.ndarray
+    objects behind each column of a DataFrame, getting or setting columns
+    in a DataFrame often results in the creation of new numpy arrays from
+    existing ones, making it non-trivial to place any array using shared
+    memory into a DataFrame without it being replaced along the way.  This
+    function takes an already existing column on a DataFrame and copies
+    its numpy.ndarray-backed contents into shared memory for subsequent
+    and continued use in the DataFrame.
+
+    If an operation is performed on a DataFrame such that yet another
+    copy of the existing column's ndarray data is constructed, that
+    column may need to be placed into shared memory again.
+
+    Be aware that because panda's BlockManager prefers to combine the
+    storage of two or more columns of the same dtype into a single
+    numpy array (with ndim>=2), requesting that one column be moved
+    to shared memory may cause all columns of that same dtype to be
+    moved as well."""
+
+    # A DataFrame's BlockManager is currently held in `_data`.
+    blkmgr = df._data
+
+    # A BlockManager holds onto instances of Block (or subtypes such
+    # as IntBlock) in a tuple named `blocks`.
+    column_NDFrame_position = blkmgr.items.get_loc(column)
+    column_blocks_position = blkmgr._blknos[column_NDFrame_position]
+    insitu_block = blkmgr.blocks[column_blocks_position]
+
+    # Sharing an ndarray of objects is not supported.
+    assert insitu_block.dtype != np.dtype('O')
+
+    # Take the existing ndarray and copy it into shared memory,
+    # replacing it in the block with its "shared" equivalent which
+    # should also be conveniently pickle-able without needing to
+    # serialize the data it holds.
+    shared_values = shareable_wrap(insitu_block.values)
+    insitu_block.values = shared_values
+
+    # Return a handle on the object in shared memory for tracking
+    # purposes so that its shared memory segment may be released later.
+    return shared_values
